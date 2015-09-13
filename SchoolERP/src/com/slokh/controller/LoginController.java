95b29a902c691d0a1ea03767c021eb3093b97b37
maneeshjha <maneesh08.jha@gmail.com>
2015-09-13 10:42:08 +0530

    Inital commit

---
+++ SchoolERP/src/com/slokh/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.slokh.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.slokh.form.LoginForm;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+	@RequestMapping(method = RequestMethod.GET)
+    public String showLoginPage(Map<String, Object> model) {
+        LoginForm loginForm	= new LoginForm();
+        model.put("loginForm", loginForm);
+		return "loginPage";
+    }
+	@RequestMapping(method = RequestMethod.POST)
+    public String getUserInformation(Map<String, Object> model) {
+        LoginForm loginForm	= new LoginForm();
+        model.put("loginForm", loginForm);
+		return "loginPage";
+    }
+}
